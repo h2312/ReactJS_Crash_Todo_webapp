2045348769ad5fa520dc01d1cd099b924cdd5395
Nguyễn Văn Hiệp <47738638+h2312@users.noreply.github.com>
2020-04-14 20:47:29 +0700

    firstcommit
    first commit

---
+++ build/precache-manifest.2cbad86f9ca2717e7669796ba3233a6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20426647e912299d71b2df7c6b9c3434",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78be42c813df7b93926c",
+    "url": "/static/css/main.2798c673.chunk.css"
+  },
+  {
+    "revision": "98d77f27a21384849014",
+    "url": "/static/js/2.bce55ff8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bce55ff8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78be42c813df7b93926c",
+    "url": "/static/js/main.cfc809b0.chunk.js"
+  },
+  {
+    "revision": "1993d187df82af029235",
+    "url": "/static/js/runtime-main.8eae6a0b.js"
+  }
+]);
